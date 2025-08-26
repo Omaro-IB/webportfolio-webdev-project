c98ce47011b416052be1feb3388d7fc70a8120e9
Omar <>
2025-08-26 11:55:33 +0300

    navbar desktop changes

--- wp_front/hosting/src/components/Navbar/NavbarDesktop.jsx
+++ wp_front/hosting/src/components/Navbar/NavbarDesktop.jsx
@@ -49,9 +49,9 @@ const NavbarDesktop = ({children, left, right}) => {
             </div>
 
             <div className={"w-sides min-w-sides sticky top-0 right-0 h-screen bg-blackshadow pt-10 overflow-clip"}>
-                <div className={"flex flex-col h-full items-center"}>
-                    <div className={"h-full text-center"}>
-                        <p className={"rotate-270 underline text-4xl text-pinkbeige mb-40 mt-24"}><HashLink to={link_right}>{right}</HashLink></p>
+                <div className={"h-full2"}>
+                    <div className={"h-fit mt-72 text-center"}>
+                        <p className={"rotate-270 underline text-4xl text-pinkbeige py-10"}><HashLink to={link_right}>{right}</HashLink></p>
                     </div>
                     <div className={"h-full"}>
                     </div>
